e0570c8b130b9890e8d9b916c7f37c790dbb3c3d
ThaneStevens <thanerstevens@gmail.com>
2020-05-06 10:23:27 -0600

    change back

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -4,7 +4,7 @@ define("DB_SERVER", "localhost");
 define("DB_USER", "harvi");
 define("DB_PASS", "qZe5cLX0Io3AjSfbm6sh0Qm$1");
 define("DB_NAME", "harvi");
-define("DEFAULT_PASS", "password");
+define("DEFAULT_PASS", "P@55word");
 define("VERSION_NUMBER", "1.5.3");
 
 ?>
